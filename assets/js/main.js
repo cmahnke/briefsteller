fa0a74b4c4a93228de48be00c4c91d865e58cd19
Christian Mahnke <cmahnke@gmail.com>
2021-04-26 00:09:11 +0200

    Animatios work smoothly in Chrome - Firefox sucks

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,15 @@ require('velocity-animate');
 require('./iiif-image-viewer');
 
 function addRotate(nodes) {
-    nodes.mouseenter(function(){
-      $(this).parent('.book-wrap').addClass('rotate');
-    }).mouseleave(function(){
-      $(this).parent('.book-wrap').removeClass('rotate');
+    /* nodes should be '.book-wrap' */
+    nodes.hover(function(){
+      $(this).addClass('rotate');
+  },function(){
+      $(this).removeClass('rotate');
     });
 }
 
 function addClick(node) {
-    console.log('adding ');
     node.click(function (event) {
 
         /* Get width of container and devide by with of item, to get items per row. Multiply width of item by two  */
@@ -21,14 +21,21 @@ function addClick(node) {
         var rowItems = Math.round(containerWidth / itemWidth);
         console.log('Width of container ' + containerWidth + ' item width ' + itemWidth + ' items per row ' + rowItems);
 
+        /* Reset already opened books - TODO: add transition */
         $('.book-li.open').removeClass('open')
-        $(this).closest('.book-li').addClass('open');
-        $(this).closest('.context').addClass('open');
+
+        $('.page').removeClass('open');
+        /* Disable mouseover animation */
+        $(this).closest('.book-wrap').unbind('mouseenter mouseleave').removeClass('rotate');
+        $(this).closest('.book-li').css('justify-content', 'flex-end').addClass('open');
+
         var link = $(this).parent('a');
         link.parent('.book-wrap').removeClass('rotate');
         link.unbind('mouseenter').unbind('mouseleave');
         link.children('.page').each(function () {
-            $(this).show().addClass('open');
+            $(this).unbind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd");
+            /* $(this).show().addClass('open'); */
+            $(this).addClass('open');
         });
         link.find('.close-book').show().css('opacity', 1);
 
@@ -41,27 +48,39 @@ function addClick(node) {
 
 $(document).ready(function() {
 
-    addRotate($('.book-link'));
+    addRotate($('.book-link').parent());
 
     $('.close-book').each(function () {
         $(this).click(function (event) {
             event.preventDefault();
-            $(this).hide().css('opacity', 0);
+            var btn = $(this);
+
             var link = $(this).closest('.book-link');
             link.attr('href', '#');
-            link.closest('.book-li.open').removeClass('open');
-            link.closest('.context.open').removeClass('open');
+
             link.children('.page').each(function () {
-                $(this).hide().removeClass('open');
+                $(this).removeClass('open').bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+                    btn.hide().css('opacity', 0);
+                    link.closest('.book-li.open').css('justify-content', 'unset').removeClass('open').bind("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+
+
+                    });
+
+                 });
             });
-            addRotate(link);
+
+            addRotate(link.parent());
             addClick(link.find('.book.preview'));
 
         });
     });
 
-    $('.book.preview').each(function () {
-        addClick($(this));
+    $('.book-link').each(function () {
+        var pageHeight = $(this).find('.book.preview img').height();
+        $(this).find('.page').each(function (){
+            $(this).height(pageHeight);
+        });
+        addClick($(this).find('.book.preview'));
     });
 
 });
